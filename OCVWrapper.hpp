2b1023988113cac1a47f8f4e475a2c515a986c63
Arnav Tiwari <34343421+arnav-t@users.noreply.github.com>
2018-02-13 13:36:27 +0000

    Add files via upload

--- OCVWrapper.hpp
+++ OCVWrapper.hpp
@@ -8,6 +8,15 @@ class OCVWrapper
 {
 private:
 	cv::Mat img;
+	bool inBounds(cv::Point p) const
+	{
+		if(p.y >= img.rows || p.y < 0)
+			return false;
+		else if(p.x >= img.cols || p.x < 0)
+			return false;
+		else 
+			return true;
+	}
 public:
 	OCVWrapper(std::string path, bool color)
 	{
@@ -30,5 +39,14 @@ public:
 	{
 		cv::waitKey(wait);
 	}
-	
+	template <class T>
+	T getPixel(cv::Point p) const
+	{
+		if(!inBounds(p))
+			return -1;
+		else if(img.channels() == 1)
+			return img.at<uchar>(p.y, p.x);
+		else if(img.channels() == 3)
+			return img.at<cv::Vec3b>(p.y, p.x);
+	}
 };
--- main.cpp
+++ main.cpp
@@ -5,7 +5,14 @@
 #include "OCVWrapper.hpp"
 #endif
 
+using namespace std;
+using namespace cv;
+
 int main()
 {
+	OCVWrapper lena("lena.jpg",1);
+	cout << (int)lena.getPixel<Vec3b>(Point(0,0))[0] << endl;
+	lena.showImg("Full image");
+	lena.update(0);
 	return 0;
 }
